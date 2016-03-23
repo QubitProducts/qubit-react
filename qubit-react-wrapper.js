56e5b5aa572c37d29e74e43f4a656abc700fe8f0
Tommy Kwan <tkwan196@gmail.com>
2016-03-23 15:05:10 +0000

    Initial commit

---
+++ qubit-react-wrapper.js
@@ -0,0 +1,44 @@
+var React = require('react')
+
+var QubitReactWrapper = React.createClass({
+  propTypes: {
+    id: React.PropTypes.string.isRequired,
+    children: React.PropTypes.node
+  },
+
+  getNamespace: function () {
+    var id = this.props.id
+    window.__qubit = window.__qubit || {}
+    window.__qubit.reactHooks = window.__qubit.reactHooks || {}
+    window.__qubit.reactHooks[id] = window.__qubit.reactHooks[id] || {}
+    return window.__qubit.reactHooks[id]
+  },
+
+  componentWillMount: function () {
+    this.getNamespace().update = this.forceUpdate
+  },
+
+  componentWillUnmount: function () {
+    delete this.getNamespace().update
+  },
+
+  render: function () {
+    var handler = this.getHandler(this.props.id)
+    if (handler) {
+      var result = handler(this.props)
+      if (typeof result === 'string') {
+        return <div dangerouslySetInnerHTML={{ __html: result }} />
+      } else {
+        return result
+      }
+    } else {
+      return this.props.children || null
+    }
+  },
+
+  getHandler: function () {
+    return this.getNamespace().handler
+  }
+})
+
+module.exports = QubitReactWrapper
