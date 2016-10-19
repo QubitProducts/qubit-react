004b1dafb8a56d24f39a3f82f9319ba83d2c08f7
Tommy Kwan <tkwan196@gmail.com>
2016-10-19 08:51:45 +0100

    Catch errors from onReactReady handlers

--- __tests__/exposeReact-test.js
+++ __tests__/exposeReact-test.js
@@ -1,30 +1,62 @@
 import exposeReact from '../lib/exposeReact'
 
-it('exposes React to the correct location', () => {
-  const fakeReact = {}
-  exposeReact(fakeReact)
-  expect(window.__qubit.reactTools.React).toBe(fakeReact)
-})
+describe('onReactReady', () => {
+  afterEach(() => {
+    window.__qubit = undefined
+  })
 
-it('runs onReactReady handlers', () => {
-  const onReactReady = [jest.fn(), jest.fn()]
-  window.__qubit = {
-    reactTools: {
-      onReactReady: onReactReady
-    }
-  }
-  exposeReact({})
-  onReactReady.forEach(fn => expect(fn).toHaveBeenCalled())
-})
+  it('exposes React to the correct location', () => {
+    const fakeReact = {}
+    exposeReact(fakeReact)
+    expect(window.__qubit.reactTools.React).toBe(fakeReact)
+  })
 
-it('runs onReactReady handlers with React', () => {
-  const fakeReact = {}
-  // Not using .toHaveBeenCalledWith because we need === match
-  const onReactReady = jest.fn(React => expect(React).toBe(fakeReact))
-  window.__qubit = {
-    reactTools: {
-      onReactReady: [onReactReady]
+  it('runs onReactReady handlers', () => {
+    const onReactReady = [jest.fn(), jest.fn()]
+    window.__qubit = {
+      reactTools: {
+        onReactReady: onReactReady
+      }
     }
-  }
-  exposeReact(fakeReact)
+    exposeReact({})
+    onReactReady.forEach(fn => expect(fn).toHaveBeenCalled())
+  })
+
+  it('runs onReactReady handlers with React', () => {
+    const fakeReact = {}
+    // Not using .toHaveBeenCalledWith because we need === match
+    const onReactReady = jest.fn(React => expect(React).toBe(fakeReact))
+    window.__qubit = {
+      reactTools: {
+        onReactReady: [onReactReady]
+      }
+    }
+    exposeReact(fakeReact)
+  })
+
+  it('catches errors thrown from onReactReady handlers', () => {
+    window.__qubit = {
+      reactTools: {
+        onReactReady: [() => { throw new Error('onReactReady error') }]
+      }
+    }
+    expect(() => {
+      exposeReact({})
+    }).not.toThrow()
+  })
+
+  it('runs all onReactReady handlers even if one throws', () => {
+    const onReactReady = [
+      jest.fn(),
+      jest.fn(() => { throw new Error('onReactReady error') }),
+      jest.fn()
+    ]
+    window.__qubit = {
+      reactTools: {
+        onReactReady: onReactReady
+      }
+    }
+    exposeReact({})
+    onReactReady.forEach(fn => expect(fn).toHaveBeenCalled())
+  })
 })
