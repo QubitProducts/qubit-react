139bcb2d7e49799da97a14f2d56a0ffd68302117
Tommy Kwan <tkwan196@gmail.com>
2016-12-19 12:59:12 +0000

    Support server side rendering

--- wrapper/index.js
+++ wrapper/index.js
@@ -1,1 +1,9 @@
-module.exports = require('./QubitReactWrapper')
+if (typeof window !== 'undefined') {
+  module.exports = require('./QubitReactWrapper')
+} else {
+  // If server side rendering, there will be nothing in the global namespace
+  // to override. So we just render the children
+  module.exports = function (props) {
+    return props.children
+  }
+}
